bc4da0b7ece52957ee3d4ef2d250f4a5b87ff035
Steven Scott Foster <stevensfoster@gmail.com>
2015-05-06 12:02:39 -0700

    added insert example

--- data_structures/arrays.rb
+++ data_structures/arrays.rb
@@ -6,7 +6,10 @@ Array.new(5)
 # A two dimentional array can be created with brace expansion
 a = Array.new(5) { Array.new(5, 0) }
 
-#An array can have a value added to the end with the push method
+# An array can have a value added to the end with the push method
 a = Array.new(5)
 a.push(nil) 
+
+# Insert an object into an array a specific index 
+a.insert(2,nil)
 
