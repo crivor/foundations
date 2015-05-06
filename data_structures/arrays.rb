b51f9ec3f60df27828ff5e4954d2e8c5767f3e17
Steven Scott Foster <stevensfoster@gmail.com>
2015-05-06 11:57:47 -0700

    exmaple showing a resizeble array with the push method

--- data_structures/arrays.rb
+++ data_structures/arrays.rb
@@ -5,3 +5,8 @@ Array.new(5)
 
 # A two dimentional array can be created with brace expansion
 a = Array.new(5) { Array.new(5, 0) }
+
+#An array can have a value added to the end with the push method
+a = Array.new(5)
+a.push(nil) 
+
