81cb3ee33d618940cde260e3d5258e3989cac16d
Steven Scott Foster <stevensfoster@gmail.com>
2015-05-07 11:30:33 -0700

    added example for queues

---
+++ data_structures/queues.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/env ruby
+
+# Queues in Ruby are also implemented as Arrays
+
+# Creates our queue
+queue = Array.new(5)
+
+# Adds another item to the queue
+queue.push(nil)
+
+# Takes the first object into the array
+queue.shift
+
